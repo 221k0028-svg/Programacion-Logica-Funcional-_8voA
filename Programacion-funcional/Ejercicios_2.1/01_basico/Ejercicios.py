8c027b0e313a85b8f41cbf806f3593247c565566
221k0028-svg <221k0028@itscarrillopuerto.edu.mx>
2026-03-18 20:48:04 -0500

    Subiendo primeros ejercicios de programacion funcional

---
+++ Programacion-funcional/Ejercicios_2.1/01_basico/Ejercicios.py
@@ -0,0 +1,5 @@
+from os import system
+if system("clear") !=0: system("cls")
+
+print("\nEjercicio 1: Imprimir mensajes")
+print("Escribe un programa que imprima tu nombre y tu ciudad en líneas separadas")
