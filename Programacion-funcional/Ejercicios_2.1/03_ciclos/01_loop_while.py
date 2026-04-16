8bf565b4d554c7c857710d032be1278b03ffc1ec
221k0028-svg <221k0028@itscarrillopuerto.edu.mx>
2026-04-15 22:58:54 -0500

    Subiendo ejercicios en aula

---
+++ Programacion-funcional/Ejercicios_2.1/03_ciclos/01_loop_while.py
@@ -0,0 +1,78 @@
+####
+# 01 - Bucle (while...)
+
+from os import system
+if system("clear") != 0: system("cls")
+
+print("\n Bucle While: ")
+# Bucle con una simple condición
+contador = 0
+
+while contador <= 5:
+    print(contador)
+    contador += 1 # es super importante para evitar bucles infinitos
+
+
+
+#Utilizando la palabras break para romper en bluce
+print("\n Bucle While con break:")
+
+contador = 0
+while True:
+    print(contador)
+    contador += 1
+    if contador == 5:
+        break # Sale del bucle
+
+
+
+# continue, que lo hace es saltar esa iteracion en concreto
+# y continuar con el bucle
+print("\n Bucle con continue:")
+
+contador = 0
+while contador < 10:
+    contador += 1
+
+    if contador % 2 == 0:
+        continue
+
+    print(contador)
+
+
+# else, esta condición cuando se ejecuta
+print("\n Bucle While con else:")
+
+contador = 0
+while contador < 5:
+    print (contador)
+    contador +=1
+
+else:
+    print("El bucle ha terminado")
+
+
+# pedirle al usuario un numero que tiene
+# que ser positivo si no, no le dejams en paz
+numero = -1
+
+while numero < 0:
+    numero = int(input("Escribe un número positivo: "))
+    if numero < 0:
+        print("El numero debe ser positivo. Intenta otra vez")
+
+print(f"El numero que has introducido es {numero}")
+
+
+numero = -1
+while numero < 0:
+    try:
+        numero = int(input("Escribe un numero positivo: "))
+        if numero < 0:
+            print("El numero debe ser positivo. Intenta otra vez")
+    except:
+        print("Lo que introduces debe ser un numero!")
+
+print(f"El numero que has introducido es {numero}")
+
+
